d3d5035f7c5f3b0120f27f2b1ef22969b45aae8c
Andrelisseth <87916047+Andrelisseth@users.noreply.github.com>
2022-06-10 10:04:07 -0600

    commit

---
+++ ProductoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using System.Net.Http;
+
+namespace WebAppBar.Controllers
+{
+    public class ProductoController : ApiController
+    {
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult Add(Models.Request.ProductoRequest model)
+
+        {
+            using (Models.BarDbEntities db = new Models.BarDbEntities())
+
+        {
+        var oProducto = new Models.Producto();
+                oProducto.nomProd= model.Nombre;
+                oProducto.descripcion = model.Descripcion;
+                oProducto.precio = model.Precio;
+                oProducto.cantidad = model.Cantidad;
+                oProducto.estado = model.Estado;
+
+                db.Producto.Add(oProducto);
+                db.SaveChanges();
+            }
+            return Ok("Hecho");
+        }
+        
+    }
+}
